d3d8f1533c97871e78697c2a173894b551ea1301
Sagar57-hk <126450990+Sagar57-hk@users.noreply.github.com>
2024-04-27 21:18:13 +0500

    2nd

--- app/src/main/java/com/project/madassignment2/MainActivity3.java
+++ app/src/main/java/com/project/madassignment2/MainActivity3.java
@@ -1,14 +1,52 @@
 package com.project.madassignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    LinearLayout portrait, landscape;
+    FragmentManager manager;
+    Fragment ftask, fDetail;
+
+    TextView tvDescription;
+
+    ArrayList<String> phoneNumbers;
+    View viewOfDetailFrag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    init();
+
+        tvDescription =  viewOfDetailFrag.findViewById(R.id.tvDesc);
+
+        phoneNumbers = new ArrayList<>();
+        phoneNumbers.add("0323312");
+        phoneNumbers.add("0868576");
+        phoneNumbers.add("0912121");
+        phoneNumbers.add("0585858");
+        phoneNumbers.add("0232323");
+
+        tvDescription.setText(phoneNumbers.get(0));
+    }
+
+    private void init()
+    {
+            portrait = findViewById(R.id.portrait_mode);
+            //landscape = findViewById(R.id.landscape_mode);
+            manager = getSupportFragmentManager();
+            ftask = manager.findFragmentById(R.id.ftask);
+            fDetail = manager.findFragmentById(R.id.fDetail);
+            assert fDetail != null;
+            viewOfDetailFrag = fDetail.requireView();
     }
 }
---
+++ app/src/main/java/com/project/madassignment2/Task.java
@@ -0,0 +1,45 @@
+package com.project.madassignment2;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Task#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Task extends ListFragment {
+
+    public Task() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+
+}
